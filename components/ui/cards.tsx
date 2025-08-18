f3c949ad4830cc09b13d229e6a996a1930ad7f9f
burrodecarga <edwinhenriquezh@hotmail.com>
2025-08-17 20:25:17 -0400

    Rep foonts

--- components/ui/cards.tsx
+++ components/ui/cards.tsx
@@ -3,15 +3,14 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+} from '@heroicons/react/24/outline'
 
-const iconMap = {
+const iconMap={
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
-};
+}
 
 export default async function CardWrapper() {
   return (
@@ -27,7 +26,7 @@ export default async function CardWrapper() {
         type="customers"
       /> */}
     </>
-  );
+  )
 }
 
 export function Card({
@@ -35,24 +34,24 @@ export function Card({
   value,
   type,
 }: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  title: string
+  value: number|string
+  type: 'invoices'|'customers'|'pending'|'collected'
 }) {
-  const Icon = iconMap[type];
+  const Icon=iconMap[type]
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+        {Icon? <Icon className="h-5 w-5 text-gray-700" />:null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
+        className={`
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
     </div>
-  );
+  )
 }
