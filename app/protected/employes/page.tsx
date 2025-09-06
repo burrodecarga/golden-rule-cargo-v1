4a4e7bbdefc29c741e93cbf0158a42c9d707cfcc
burrodecarga <edwinhenriquezh@hotmail.com>
2025-09-06 10:19:56 -0400

    card Chofer

--- app/protected/employes/page.tsx
+++ app/protected/employes/page.tsx
@@ -1,8 +1,22 @@
+import ListActivities from '@/components/list-activities'
+import { getServiciosByChofer } from '@/lib/api'
+import { createClient } from '@/lib/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-export default function EmployesRoute() {
+export default async function EmployesRoute() {
+
+    const supabase=createClient()
+    const { data, error }=await (await supabase).auth.getUser()
+    if (error||!data?.user) {
+        redirect('/auth/login')
+    }
+
+    const serviciosByChofer=await getServiciosByChofer(data.user.id)
+
+    console.log('SERVICIOS DEL CHOFER', serviciosByChofer)
 
     return (
-        <div>EmployesRoute</div>
+        <ListActivities activities={serviciosByChofer} filter={3} />
     )
 }
--- components/list-activities.tsx
+++ components/list-activities.tsx
@@ -1,28 +1,57 @@
-import { FetchServicios } from '@/lib/api'
-import React from 'react'
-import { Button } from './ui/button'
-import Link from 'next/link'
+import { FetchServicios } from "@/lib/api"
+import React from "react"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import { formatCurrency, formatDateToLocal } from "@/helpers/utils"
+import { Separator } from "@radix-ui/react-separator"
 
-export default function ListActivities({ activities, filter }: { activities: FetchServicios, filter: number }) {
-
-
-
-
+export default function ListActivities({
+    activities,
+    filter
+}: {
+    activities: FetchServicios
+    filter: number
+}) {
     return (
-        <div>
-            {activities&&activities.map((a) => {
-                return (
-                    <div className="flex flex-col w-full" key={a.id+a.orden} >
-                        <Link href={`/protected/chofer/${a.id}`}>
-                            <Button className='px-4 py-2 bg-blue-500 w-full mb-3'>
-                                Route: {a.ruta} - Price: {a.precio_de_servicio} $
-
-                            </Button>
-                        </Link>
-
-                    </div>
-                )
-            })}
+        <div className='w-full p-5'>
+            <h4 className='w-full mx-auto text-sm leading-none font-medium'>Trips</h4>
+            <div className='flex flex-row gap-3 rounded-md border border-gray-50 p-5 bg-red-100'>
+                {activities&&
+                    activities
+                        .sort((a, b) => (a.start!>b.start!? -1:1))
+                        .map((a) => (
+                            <React.Fragment key={a.id}>
+                                <Card className='flex-1'>
+                                    <CardHeader>
+                                        <CardTitle className='space-y-2'>
+                                            <p>{a.ruta?.toUpperCase()}</p>
+                                            <p>{formatDateToLocal(a.start!)}</p>
+                                            <p>Broker:{a.broker?.toUpperCase()}</p>
+                                            <p>Dispacher:{a.despachador?.toUpperCase()}</p>
+                                            <p>Price:{formatCurrency(a.precio_de_servicio!)}</p>
+                                            <p>Pay:{a.forma_de_pago}</p>
+                                        </CardTitle>
+                                        <CardDescription>
+                                            <p>Origin:{a.origen}</p>
+                                            <p>{a.destino}</p>
+                                        </CardDescription>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <p>Payload:{a.carga}</p>
+                                    </CardContent>
+                                    <CardFooter>
+                                        <p>Card Footer</p>
+                                    </CardFooter>
+                                </Card>
+                            </React.Fragment>
+                        ))}
+            </div>
         </div>
     )
 }
